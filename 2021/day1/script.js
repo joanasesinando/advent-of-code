f6e73ed59a9f3c14f4aedc03774815aa161a4cce
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2021-12-02 06:27:40 +0000

    day 1

---
+++ 2021/day1/script.js
@@ -0,0 +1,33 @@
+function runDay1(input) {
+    input = input.split("\n").map((n) => parseInt(n, 10));
+
+    const res1 = runP1();
+    const res2= runP2();
+    showAlert("success", "Result - p1: " + res1 + "<br>Result - p2: " + res2);
+
+    function runP1() {
+        let res = 0, previous = null;
+    
+        input.forEach((n) => {
+            if (previous && n > previous) res++;
+            previous = n;
+        });
+
+        return res;
+    }
+    
+    function runP2() {          
+        let res = 0, previous = null;
+    
+        input.forEach((n, i, arr) => {
+            if (i > arr.length - 3) return;
+    
+            const total = n + arr[i + 1] + arr[i + 2];
+    
+            if (previous && total > previous) res++;
+            previous = total;
+        });
+
+        return res;
+    }
+}
